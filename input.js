4b51b6963c88baf539b6b6c1e4971c1e5b8fe672
Jeremy <010JGL@GMAIL.COM>
2023-03-15 22:06:44 -0400

    added movements

--- input.js
+++ input.js
@@ -1,19 +1,41 @@
+const { connect } = require("./client");
+const net = require("net");
 
+// Stores the active TCP connection object.
+let connection;
 
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   // event listener
-  stdin.on("data", handleUserInput);
+  stdin.on("data", key => handleUserInput(key));
+  
   return stdin;
 };
 
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
+  }
+  if (key === 'w') {
+    console.log('Move: up');
+    connection.write('Move: up');
+  }
+  if (key === 'a') {
+    console.log('Move: left');
+    connection.write('Move: left');
+  }
+  if (key === 's') {
+    console.log('Move: down');
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    console.log('Move: right');
+    connection.write('Move: right');
   }
 };
 
--- play.js
+++ play.js
@@ -3,6 +3,8 @@ const { connect } = require("./client");
 const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
 
-setupInput();
+//connect();
+
+setupInput(connect());
+
