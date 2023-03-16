d317267b18bf9970a79f2be437ec021c2f54039a
Jeremy <010JGL@GMAIL.COM>
2023-03-15 20:20:05 -0400

    Sending msg to server

--- client.js
+++ client.js
@@ -10,19 +10,17 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding("utf8");
   //allows to receive data from the server, and log it back to us
-  conn.on('data', (messageFromServer) => {
+  conn.on('data', (messageFromServer) => {   // msg form server = argument of callback
     console.log('The server says :', messageFromServer);
   });
-  conn.on('connect', (connect) => {
+  conn.on('connect', () => {
     console.log('Successfully connected to game server');
   });
+  
   conn.write("Name: J4Y");
-  
-  
-
   return conn;
 };
-
+//conn.write("Move: up");
 
 module.exports = {
   connect 
